6686fefa240378dcb70a351e65570b43f1ca7b77
echo "Jasper <89773560+jasperveraart@users.noreply.github.com>
2023-02-27 14:21:53 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// var website = "https://maze-mapping.com";
+// var studentName = "Jasper Veraart";
+//
+// let apples = 1;
+// const height = "1,80";
+//
+// console.log(website, studentName)
+
+// let list = ["test", "test2", 3]
+// console.log(list)
+// list.push("New item in the back")
+// list.pop()
+// console.log(list)
+
+// const arr = ["Zero", "One", "Two", "Three"]
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+//
+// arr.forEach(num => {
+//     console.log(num);
+// })
+//
+// const elems = document.querySelectorAll(".class");
+// console.log(elems)
+
+//
+function calcNumbers(param1, param2) {
+    const result =  param1 + param2
+
+    return result
+}
+
+calcNumbers(1, 2)
